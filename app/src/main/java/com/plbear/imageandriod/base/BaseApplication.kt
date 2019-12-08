563fe43a4d0141a6f3010abb4fe0b2904dc5501d
闫永俊 <yanyojun@foxmail.com>
2019-12-08 10:33:06 +0800

    Initial commit

---
+++ app/src/main/java/com/plbear/imageandriod/base/BaseApplication.kt
@@ -0,0 +1,17 @@
+package com.plbear.imageandriod.base
+
+import android.app.Application
+
+/**
+ * created by yanyongjun on 2019-12-08
+ */
+class BaseApplication : Application() {
+    companion object {
+        lateinit var instance: Application
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+}
